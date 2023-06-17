31695ec7d2a3248886042a44379a31be1e108393
alvarado0925 <alvarado0925@gmail.com>
2023-06-16 23:24:26 -0500

    Install TailwindCSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,9 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
 <body>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Mi botón
+    </button>
+
     @foreach ($posts as $post)
         <div>
             <h2>{{ $post->title }}</h2>
